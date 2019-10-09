688654dfb933cdaf506d4e02d64ed4c3acec4f41
sidney gerst <you@example.com>
2019-10-09 14:23:30 -0700

    gpx to csv with loop

--- gpx_to_csv_test.R
+++ gpx_to_csv_test.R
@@ -1,4 +1,4 @@
-#Converting GPX files to CSV
+#Converting GPX files to CSV for the 2017 folder
 library(plotKML)
 library(tidyverse)
 library(janitor)
@@ -8,14 +8,32 @@ library(rgdal)
 #first, set the wd to the vesseltracksightings
 setwd("C:/Users/sidg0/Documents/Bren/WorthWhale/Research_Vessel_Tracks/WorthWhale_vesseltracksightings/2017")
 gpx_sightings2017 <- list.files() #list the files within the 2017 folder 
-gpx_raw <- readGPX(gpx_sightings2017[2:4],
-                   metadata = FALSE,
-                   bounds = FALSE,
-                   waypoints = FALSE,
-                   routes = FALSE)
+# Initialise empty data frame -- set up for loop
+gpx_full <- NULL
 
-head(gpx_raw)
+for (i in 1:length(gpx_sightings2017)[1]) {
+  # - Select first file from the list and import data into R object
+  tracklist <- readGPX(gpx_sightings2017[i], metadata = FALSE,
+                     bounds = FALSE,
+                     waypoints = FALSE,
+                     routes = FALSE)
+  # extract latitude, longituDe, elevation, time, name and comments and apppend to R dataframe
+  tracksdf<- tracklist$tracks
+  # append dataframe from last index to a full waypoint object
+  gpx_full <- bind_rows(gpx_full, tracksdf)
+}
+head(gpx_full)
+#TEST
+# gpx_raw <- readGPX(gpx_sightings2017[2:4],
+#                    metadata = FALSE,
+#                    bounds = FALSE,
+#                    waypoints = FALSE,
+#                    routes = FALSE)
 
+#TEST
+#head(gpx_raw)
+
+#TEST
 # gpx_filepath = file.path("C:/Users/sidg0/Documents/Bren/WorthWhale/Research_Vessel_Tracks/WorthWhale_vesseltracksightings/2017", gpx_sightings2017)
 # gpx_dataset <- do.call("rbind",lapply(gpx_filepath,FUN=function(files){ readGPX(files, metadata = FALSE,
 #                                                                                 bounds = FALSE,
@@ -24,16 +42,16 @@ head(gpx_raw)
 
 #i= gpx_sightings2017[1:5] Need to figure out how to loop all the gpx files into the code frew provided
 
-
+#from Frew
 # g = readGPX("2017-03-31 20.50.14 Day.gpx",
 #             metadata = FALSE,
 #             bounds = FALSE,
 #             waypoints = FALSE,
 #             routes = FALSE)
 
+#from frew
 track_points = Reduce(rbind, gpx_raw$tracks[[1]])
-
 head(track_points)
 
 #write csv
-write.csv(track_points,paste0() file = "GPXtoCSV.csv")
+write.csv(track_points, file = "GPXtoCSV.csv")
