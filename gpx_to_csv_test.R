4d49cd550dfb3ac4a0356e04cb8deb238d36c96c
Laura Ingulsrud <lauraingulsrud@esm.ucsb.edu>
2019-10-29 11:09:51 -0700

    Deleted unnecessary shell scripts

--- gpx_to_csv_test.R
+++ gpx_to_csv_test.R
@@ -6,41 +6,31 @@ library(rgdal)
 
 
 #first, set the wd to the vesseltracksightings
-setwd("C:/Users/sidg0/Documents/Bren/WorthWhale/Research_Vessel_Tracks/WorthWhale_vesseltracksightings/2017")
-gpx_sightings2017 <- list.files() #list the files within the 2017 folder 
-# Initialise empty data frame -- set up for loop
-gpx_full <- NULL
 
-for (i in 1:length(gpx_sightings2017)[1]) {
+gpx_sightings2017 <- list.files("2017/", full.names = T) #list the files within the 2017 folder 
+# Initialise empty data frame -- set up for loop
+first_time <- T
+
+for (gpx_file in gpx_sightings2017) {
+  print(gpx_file)
   # - Select first file from the list and import data into R object
-  tracklist <- readGPX(gpx_sightings2017[i], metadata = FALSE,
+  tracklist <- readGPX(gpx_file, metadata = FALSE,
                      bounds = FALSE,
                      waypoints = FALSE,
                      routes = FALSE)
   # extract latitude, longituDe, elevation, time, name and comments and apppend to R dataframe
   tracksdf<- tracklist$tracks
   # append dataframe from last index to a full waypoint object
-  gpx_full <- bind_rows(gpx_full, tracksdf)
+  if (first_time) {
+    gpx_full <- tracksdf
+    first_time <- F
+  }
+  else {
+    gpx_full <- rbind(gpx_full, tracksdf)
+  }
 }
-head(gpx_full)
-#TEST
-# gpx_raw <- readGPX(gpx_sightings2017[2:4],
-#                    metadata = FALSE,
-#                    bounds = FALSE,
-#                    waypoints = FALSE,
-#                    routes = FALSE)
+#head(gpx_full)
 
-#TEST
-#head(gpx_raw)
-
-#TEST
-# gpx_filepath = file.path("C:/Users/sidg0/Documents/Bren/WorthWhale/Research_Vessel_Tracks/WorthWhale_vesseltracksightings/2017", gpx_sightings2017)
-# gpx_dataset <- do.call("rbind",lapply(gpx_filepath,FUN=function(files){ readGPX(files, metadata = FALSE,
-#                                                                                 bounds = FALSE,
-#                                                                                 waypoints = FALSE,
-#                                                                                 routes = FALSE)}))
-
-#i= gpx_sightings2017[1:5] Need to figure out how to loop all the gpx files into the code frew provided
 
 #from Frew
 # g = readGPX("2017-03-31 20.50.14 Day.gpx",
